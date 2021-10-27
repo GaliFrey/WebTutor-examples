4d417d8a4a2ba8e54999fc0c5e3fe3ccb4b40f16
Загоскин Владислав Андреевич <zagoskin2013@gmail.com>
2021-10-27 20:13:45 +0300

    Добавлена новая статья
    - статья "Коробочные объекты"

---
+++ Коробочные объекты/ms_tools.js
@@ -0,0 +1,5 @@
+﻿ms_tools = {
+    raise_system_event(code, system_eventID, objectDocID, objectDoc) {
+        return 1;
+    }
+}
---
+++ Коробочные объекты/tools_web.js
@@ -0,0 +1,40 @@
+﻿/**
+ * Библиотека Tools_web
+ */
+tools_web = {};
+
+tools_web.get_object_image_url = function (oObjectParam, bDispDefaultParam) {
+    return ''
+}
+
+tools_web.set_base_url_path = function (sUrlParam, oEnvParam) {
+    return ''
+}
+
+tools_web.is_true = function (oParam) {
+    return true
+}
+
+tools_web.get_web_const = function (sNameParam, fldLngParam) {
+    return ''
+}
+
+tools_web.get_cur_lng_name = function (sTextParam, sShortIDParam) {
+    return ''
+}
+
+tools_web.set_var_eval = function (sVarNamaParam, curVars, oEvalParam, sEvalTypeParam) {
+    return {}
+}
+
+tools_web.update_object_from_context = function (docObjectParam, oContextParam, arrFieldNamesParam, sPrefixParam) {
+}
+
+tools_web.custom_elems_filling = function (fldTarget, oSourceParam, arrCustomElemsParam, oParams) {
+}
+
+tools_web.save_cur_object_doc = function (docObjectParam, oEnvParam) {
+}
+
+tools_web.convert_xhttp_res = function (_text_area, _source_desc) {
+}
