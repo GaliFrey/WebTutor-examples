a7d7ed870e24b21a6e2bf8162ad48c9ca6b7acc7
Загоскин Владислав Андреевич <zagoskin2013@gmail.com>
2021-12-23 23:38:29 +0300

    Добавлен файл с объектом XmlDoc

---
+++ Коробочные объекты/xml_doc.js
@@ -0,0 +1,191 @@
+/**
+ * [Документация]{@link https://news.websoft.ru/_wt/wiki_base/6751742522177031794}
+ */
+XmlDoc = {}
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276905286592714}
+ */
+XmlDoc.AutoSave = false;
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620250451197911772}
+ */
+XmlDoc.DocID = 0;
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276905286592709}
+ */
+XmlDoc.Form = XmlForm;
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620250451197911800}
+ */
+XmlDoc.FormUrl = '';
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276905286592710}
+ */
+XmlDoc.IsChanged = false;
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276905286592719}
+ */
+XmlDoc.IsSeparated = false;
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276905286592715}
+ */
+XmlDoc.LastSavedData = XmlDoc;
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276905286592711}
+ */
+XmlDoc.NeverSaved = false;
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276905286592708}
+ */
+XmlDoc.ObjectID = 0;
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5665465792879477226}
+ */
+XmlDoc.OptScreen = Screen;
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276905286592718}
+ */
+XmlDoc.RunActionOnSave = false;
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5665465792879477225}
+ */
+XmlDoc.Screen = Screen;
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276892448878820}
+ */
+XmlDoc.TopElem = XmlElem;
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276892448878827}
+ */
+XmlDoc.Url = '';
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276905286592712}
+ */
+XmlDoc.WriteAllNodes = false;
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276905286592717}
+ */
+XmlDoc.WriteAsync = false;
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276905286592570}
+ */
+XmlDoc.WriteDocInfo = false;
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276905286592716}
+ */
+XmlDoc.WriteFt = false;
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276905286592713}
+ */
+XmlDoc.WriteMaskLineBreaks = false;
+
+
+
+/**
+ * [Документация]{@link https://news.websoft.ru/_wt/wiki_base/6954247116231617172}
+ */
+XmlDoc.BindToDb = function(arg1) {}
+
+/**
+ * [Документация]{@link https://news.websoft.ru/_wt/wiki_base/6954247395486688267}
+ */
+XmlDoc.BindToDbObjectType = function(dbName, objectType) {}
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276905286592731}
+ */
+XmlDoc.EvalThread = function(code) {}
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276905286592721}
+ */
+XmlDoc.FindExtDataElemByFieldID = function(extID) {return XmlElem}
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276905286592732}
+ */
+XmlDoc.KillActiveThread = function() {}
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276905286592727}
+ */
+XmlDoc.LoadFromLds = function() {}
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276905286592730}
+ */
+XmlDoc.MakeReport = function() {}
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276905286592726}
+ */
+XmlDoc.PrepareLastSavedData = function() {}
+
+/**
+ * [Документация]{@link https://news.websoft.ru/_wt/wiki_base/6753557626948037739}
+ */
+XmlDoc.Save = function(docUrl) {}
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276905286592728}
+ */
+XmlDoc.SaveToLds = function() {}
+
+/**
+ * [Документация]{@link https://news.websoft.ru/_wt/wiki_base/6753528884510418553}
+ */
+XmlDoc.SaveToStream = function(destStream, options) {}
+
+/**
+ * [Документация]{@link https://news.websoft.ru/_wt/wiki_base/6753528425634804971}
+ */
+XmlDoc.SaveToUrl = function(destUrl, options) {}
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276905286592722}
+ */
+XmlDoc.SetCached = function() {}
+
+/**
+ * [Документация]{@link https://news.websoft.ru/_wt/wiki_base/6753566303697393650}
+ */
+XmlDoc.SetChanged = function(isChanged) {}
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276905286592723}
+ */
+XmlDoc.SetShared = function() {}
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276892448878811}
+ */
+XmlDoc.SubElem = function(path) {return XmlElem}
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276905286592720}
+ */
+XmlDoc.TopOrChildElem = function(elemName) {return XmlElem}
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276892448878826}
+ */
+XmlDoc.UpdateValues = function() {}
