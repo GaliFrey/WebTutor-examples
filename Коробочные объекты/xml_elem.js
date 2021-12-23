d3c786e0fd5c02893cd54404b409ce895f637e07
Загоскин Владислав Андреевич <zagoskin2013@gmail.com>
2021-12-24 00:33:35 +0300

    Добавлен файл с объектом XmlElem

---
+++ Коробочные объекты/xml_elem.js
@@ -0,0 +1,476 @@
+/**
+ * [Документация]{@link https://news.websoft.ru/_wt/wiki_base/6751742736843702148}
+ */
+XmlElem = {}
+
+
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620250451197911715}
+ */
+XmlElem.AttrNames = [];
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276905286592743}
+ */
+XmlElem.BaseMultipleElem = XmlElem;
+
+/**
+ * [Документация]{@link https://news.websoft.ru/_wt/wiki_base/6951382076496946038}
+ */
+XmlElem.ChildIndex = 0;
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620250451197911736}
+ */
+XmlElem.ChildNum = 0;
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276905286592744}
+ */
+XmlElem.Doc = XmlElem;
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276905286592748}
+ */
+XmlElem.ExternalDispName = '';
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620250451197911796}
+ */
+XmlElem.ForeignArray = {};
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276905286592752}
+ */
+XmlElem.ForeignArrayCodeStr = '';
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276905286592756}
+ */
+XmlElem.ForeignDispName = '';
+
+/**
+ * [Документация]{@link https://news.websoft.ru/_wt/wiki_base/6778836091756309895}
+ */
+XmlElem.ForeignElem = {};
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276905286592754}
+ */
+XmlElem.ForeignObjectUrl = '';
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276905286592755}
+ */
+XmlElem.ForeignPrimaryObjectUrl = '';
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276905286592738}
+ */
+XmlElem.Form = XmlForm;
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276905286592736}
+ */
+XmlElem.FormElem = XmlFormElem;
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620250451197911799}
+ */
+XmlElem.FormPath = '';
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276892448878626}
+ */
+XmlElem.HasValue = false;
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276905286592739}
+ */
+XmlElem.ImageUrl = '';
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276892448878638}
+ */
+XmlElem.Instances = [];
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276892448878647}
+ */
+XmlElem.IsFirstSibling = false;
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276892448878648}
+ */
+XmlElem.IsLastSibling = false;
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276892448878649}
+ */
+XmlElem.IsMultiElem = false;
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276905286592742}
+ */
+XmlElem.IsSecondary = false;
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276905286592741}
+ */
+XmlElem.IsTemp = false;
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276892448878652}
+ */
+XmlElem.IsTopElem = false;
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276892448878680}
+ */
+XmlElem.Name = '';
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276892448878682}
+ */
+XmlElem.NextSibling = XmlElem;
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276905286592749}
+ */
+XmlElem.ObjectUrl = '';
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276905286592745}
+ */
+XmlElem.OptDoc = XmlDoc;
+
+/**
+ * [Документация]{@link https://news.websoft.ru/_wt/wiki_base/6934960024317591784}
+ */
+XmlElem.OptForeignElem = {};
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276905286592758}
+ */
+XmlElem.OptScreen = Screen;
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276892448878700}
+ */
+XmlElem.Parent = XmlElem;
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276892448878711}
+ */
+XmlElem.PrevSibling = XmlElem;
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276905286592747}
+ */
+XmlElem.PrimaryDispName = '';
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276892448878712}
+ */
+XmlElem.PrimaryKey = 0;
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276905286592750}
+ */
+XmlElem.PrimaryObjectUrl = '';
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5791375928854454879}
+ */
+XmlElem.ReadOnly = false;
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276892448878752}
+ */
+XmlElem.Screen = Screen;
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276892448878765}
+ */
+XmlElem.Size = 0;
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276905286592737}
+ */
+XmlElem.Title = '';
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276892448878822}
+ */
+XmlElem.Type = '';
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276905286592759}
+ */
+XmlElem.UseCData = false;
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276905286592514}
+ */
+XmlElem.Value = '';
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276905286592517}
+ */
+XmlElem.Xml = '';
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276905286592757}
+ */
+XmlElem.XmlValue = '';
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276905286592522}
+ */
+XmlElem.XQueryLiteral = '';
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620250451197911679}
+ */
+XmlElem.AddAttr = function(name, value) {}
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620250451197911680}
+ */
+XmlElem.AddChild = function(name, type) {return XmlElem}
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276905286592765}
+ */
+XmlElem.AddChildElem = function(elem) {}
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620250451197911681}
+ */
+XmlElem.AddDynamicChild = function(name, type) {return XmlElem}
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620250451197911713}
+ */
+XmlElem.AssignElem = function(srcElem) {}
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5652176266853304326}
+ */
+XmlElem.AssignExtraElem = function(srcElem) {}
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620250451197911731}
+ */
+XmlElem.Child = function(arg1) {return XmlElem}
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620250451197911732}
+ */
+XmlElem.ChildByKeyExists = function(keyValue, keyName) {}
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5652176266853304323}
+ */
+XmlElem.ChildByKeyExistsRec = function(keyValue) {}
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620250451197911733}
+ */
+XmlElem.ChildByValueExists = function(elemVal) {}
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620250451197911734}
+ */
+XmlElem.ChildExists = function(name) {}
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276905286592762}
+ */
+XmlElem.ChildValue = function(name) {}
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620250451197911737}
+ */
+XmlElem.Clear = function() {}
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620250451197911739}
+ */
+XmlElem.Clone = function() {return XmlElem}
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620250451197911759}
+ */
+XmlElem.Delete = function() {}
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276905286592766}
+ */
+XmlElem.DeleteAttr = function(attrName) {}
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620250451197911762}
+ */
+XmlElem.DeleteChildByKey = function(keyValue, keyName) {}
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620250451197911763}
+ */
+XmlElem.DeleteChildren = function(qualExpr) {}
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276905286592767}
+ */
+XmlElem.DeleteOptAttr = function(attrName) {}
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5652176266853304327}
+ */
+XmlElem.EqualToElem = function(elem2) {}
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276905286592764}
+ */
+XmlElem.EvalMultiPath = function(path) {}
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620250451197911784}
+ */
+XmlElem.EvalPath = function(path) {return XmlElem}
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276905286592763}
+ */
+XmlElem.FormChildExists = function(name) {}
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5652176266853304325}
+ */
+XmlElem.GetChildByAttrValue = function(attrName, attrValue) {return XmlElem}
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276892448878593}
+ */
+XmlElem.GetChildByKey = function(keyValue, keyName) {return XmlElem}
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276892448878615}
+ */
+XmlElem.GetOptChildByKey = function(keyValue, keyName) {return XmlElem}
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5652176266853304322}
+ */
+XmlElem.GetOptChildByKey = function(keyValue, keyName) {return XmlElem}
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276892448878616}
+ */
+XmlElem.GetStr = function() {}
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5791375928854454874}
+ */
+XmlElem.GetXml = function(options) {}
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276892448878637}
+ */
+XmlElem.InsertChild = function(index, name, type) {return XmlElem}
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276892448878658}
+ */
+XmlElem.LoadData = function(data) {}
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276892448878659}
+ */
+XmlElem.LoadDataFromUrl = function(url) {}
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276892448878661}
+ */
+XmlElem.LoadFromFile = function(url) {}
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276892448878662}
+ */
+XmlElem.LoadFromStr = function(data) {}
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276892448878683}
+ */
+XmlElem.ObtainAttr = function(attrName, attrValue) {}
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276892448878687}
+ */
+XmlElem.ObtainChildByKey = function(keyValue, keyName) {return XmlElem}
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5652176266853304324}
+ */
+XmlElem.ObtainChildByValue = function(elemVal) {return XmlElem}
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276892448878695}
+ */
+XmlElem.OptAttrValue = function(attrName, defaultVal) {}
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276905286592761}
+ */
+XmlElem.OptChild = function(name) {return XmlElem}
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5652176266853304320}
+ */
+XmlElem.OptChildAttrValue = function(attrName, attrValue, attrValue2) {}
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276892448878706}
+ */
+XmlElem.PathExists = function(path) {}
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276892448878714}
+ */
+XmlElem.PropertyExists = function(arg1) {}
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276892448878736}
+ */
+XmlElem.RelativePath = function() {}
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276892448878751}
+ */
+XmlElem.SaveToFile = function(url) {}
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276892448878757}
+ */
+XmlElem.SetChildIndex = function(newPos) {}
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276892448878759}
+ */
+XmlElem.SetOptAttrValue = function(attrName, attValue, defaultVal) {}
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5620276892448878771}
+ */
+XmlElem.Sort = function(elemExp1, order1) {}
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5652176266853304329}
+ */
+XmlElem.UpdateSecondaryData = function() {}
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5652176266853304328}
+ */
+XmlElem.UpdateValues = function() {}
