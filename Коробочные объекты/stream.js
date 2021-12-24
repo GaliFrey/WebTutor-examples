96d6a66bc8db181b894095c37d386af4325c282a
Загоскин Владислав Андреевич <zagoskin2013@gmail.com>
2021-12-24 12:36:03 +0300

    Добавлен файл с объектом Stream

---
+++ Коробочные объекты/stream.js
@@ -0,0 +1,19 @@
+/**
+ * [Документация]{@link https://news.websoft.ru/_wt/wiki_base/6751741852966527336}
+ */
+Stream = {};
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5665465792879477151}
+ */
+Stream.ReadLine = function() {};
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5665465792879477153}
+ */
+Stream.WriteBinary = function(binaryObj) {};
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5665465792879477152}
+ */
+Stream.WriteStr = function(str) {};
