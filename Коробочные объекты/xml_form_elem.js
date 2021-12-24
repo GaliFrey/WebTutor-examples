41ca98534f6b5a183bef5af5c986b71169b20e85
Загоскин Владислав Андреевич <zagoskin2013@gmail.com>
2021-12-24 12:05:50 +0300

    Добавлен файл с объектом XmlFormElem

---
+++ Коробочные объекты/xml_form_elem.js
@@ -0,0 +1,164 @@
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5791375928854454846}
+ */
+XmlFormElem = {};
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5791375928854454835}
+ */
+XmlFormElem.AllowHide = '';
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5791375928854454828}
+ */
+XmlFormElem.ColAlign = '';
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5791375928854454827}
+ */
+XmlFormElem.ColTitle = '';
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5791375928854454830}
+ */
+XmlFormElem.DefaultValue = '';
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5791375928854454831}
+ */
+XmlFormElem.ExpMaxLen = 0;
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5791375928854454841}
+ */
+XmlFormElem.Expr = '';
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5791375928854454823}
+ */
+XmlFormElem.ExprInit = '';
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5791375928854454829}
+ */
+XmlFormElem.ForeignArrayExpr = '';
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5791375928854454825}
+ */
+XmlFormElem.FtUseValue = '';
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5791375928854454836}
+ */
+XmlFormElem.IsIndexed = false;
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5791375928854454819}
+ */
+XmlFormElem.IsMethod = false;
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5791375928854454811}
+ */
+XmlFormElem.IsMultiple = false;
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5791375928854454832}
+ */
+XmlFormElem.IsTemp = false;
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5791375928854454817}
+ */
+XmlFormElem.Name = '';
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5791375928854454818}
+ */
+XmlFormElem.NullFalse = false;
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5791375928854454814}
+ */
+XmlFormElem.OptPrimaryDispName = '';
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5791375928854454833}
+ */
+XmlFormElem.ResultDataType = '';
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5791375928854454820}
+ */
+XmlFormElem.Title = '';
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5791375928854454837}
+ */
+XmlFormElem.TopElem = XmlFormElem;
+
+/**
+ * [Документация]{@link a}
+ */
+XmlFormElem.Type = '';
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5791375928854454834}
+ */
+XmlFormElem.Xml = '';
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5791375928854454838}
+ */
+XmlFormElem.AddChild = function(name, type) {return XmlFormElem;};
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5791375928854454812}
+ */
+XmlFormElem.Child = function(arg1) {return XmlFormElem;};
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5791375928854454840}
+ */
+XmlFormElem.ChildExists = function(name) {};
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5791375928854454824}
+ */
+XmlFormElem.EvalPath = function(path) {return XmlElem;};
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5791375928854454813}
+ */
+XmlFormElem.GetBoolAttr = function() {};
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5791375928854454815}
+ */
+XmlFormElem.GetOptAttr = function() {};
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5791375928854454842}
+ */
+XmlFormElem.Inherit = function() {return XmlFormElem;};
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5791375928854454822}
+ */
+XmlFormElem.IsMultipleRecChild = function(elem) {};
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5791375928854454854}
+ */
+XmlFormElem.LoadChildTags = function(strXml) {return XmlFormElem;};
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5791375928854454877}
+ */
+XmlFormElem.OptChild = function(name) {return XmlFormElem;};
+
+/**
+ * [Документация]{@link http://docs.datex.ru/article.htm?id=5791375928854454821}
+ */
+XmlFormElem.PathExists = function(path) {};
